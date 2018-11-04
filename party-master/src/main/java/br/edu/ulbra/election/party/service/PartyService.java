f0a000876fa2885e17a25a37eb0e36093fbf7348
alexandrebard <39573844+alexandrebard@users.noreply.github.com>
2018-11-04 21:32:15 -0200

    Update CRUD Party

--- party-master/src/main/java/br/edu/ulbra/election/party/service/PartyService.java
+++ party-master/src/main/java/br/edu/ulbra/election/party/service/PartyService.java
@@ -2,6 +2,14 @@ package br.edu.ulbra.election.party.service;
 
 import br.edu.ulbra.election.party.output.v1.PartyOutput;
 import br.edu.ulbra.election.party.repository.PartyRepository;
+import br.edu.ulbra.submissoes.exception.UserException;
+import br.edu.ulbra.election.party.exception.GenericOutputException;
+import br.edu.ulbra.election.party.input.v1.PartyInput;
+import br.edu.ulbra.election.party.model.Party;
+import br.edu.ulbra.election.party.output.v1.GenericOutput;
+import br.edu.ulbra.election.party.output.v1.PartyOutput;
+
+import org.apache.commons.lang.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +22,9 @@ import java.util.List;
 public class PartyService {
     private final PartyRepository partyRepository;
     private final ModelMapper modelMapper;
+    
+    private static final String MESSAGE_INVALID_ID = "Invalid id";
+    private static final String MESSAGE_VOTER_NOT_FOUND = "Voter not found";
 
     @Autowired
     public PartyService(PartyRepository partyRepository, ModelMapper modelMapper) {
@@ -24,6 +35,79 @@ public class PartyService {
     public List<PartyOutput> getAll(){
         Type partyOutputListType = new TypeToken<List<PartyOutput>>(){}.getType();
         return modelMapper.map(partyRepository.findAll(), partyOutputListType);
+    }
+    
+    public PartyOutput create(PartyInput partyInput) {
+        validateInput(partyInput, false);
+        Party party = modelMapper.map(partyInput, Party.class);
+        party = partyRepository.save(party);
+        return modelMapper.map(party, PartyOutput.class);
+    }
+
+    public PartyOutput getById(Long partyId){
+        if (partyId == null){
+            throw new GenericOutputException(MESSAGE_INVALID_ID);
+        }
+
+        Party party = partyRepository.findById(partyId).orElse(null);
+        if (party == null){
+            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+        }
+
+        return modelMapper.map(party, PartyOutput.class);
+    }
+
+    public PartyOutput update(Long partyId, PartyInput partyInput) {
+        if (partyId == null){
+            throw new GenericOutputException(MESSAGE_INVALID_ID);
+        }
+        validateInput(partyInput, true);
+
+        Party party = partyRepository.findById(partyId).orElse(null);
+        if (party == null){
+            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+        }
+
+        party.setCode(partyInput.getCode());
+        party.setName(partyInput.getName());
+        party.setNumber(partyInput.getNumber());
+        
+        party = partyRepository.save(party);
+        return modelMapper.map(party, PartyOutput.class);
+    }
+
+    public GenericOutput delete(Long partyId) {
+        if (partyId == null){
+            throw new GenericOutputException(MESSAGE_INVALID_ID);
+        }
+
+        Party party = partyRepository.findById(partyId).orElse(null);
+        if (party == null){
+            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+        }
+
+        partyRepository.delete(party);
+
+        return new GenericOutput("Party deleted");
+    }
+
+    private void validateInput(PartyInput partyInput, boolean isUpdate) {
+    	
+    	
+    	if (StringUtils.isBlank(partyInput.getCode())){
+            throw new GenericOutputException("Invalid code");
+        }
+        if (StringUtils.isBlank(partyInput.getName())){
+            throw new GenericOutputException("Invalid name");
+        }
+        
+        //esse tem que ajustar
+        if (StringUtils.isBlank(partyInput.getNumber())){
+            throw new GenericOutputException("Invalid number");
+        }
+        
+        
+        
     }
 
 }
