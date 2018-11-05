0c622f038a79f51a982ee5a665a9dbcb19c758bb
Adriano Saadeh <adrianosaadeh@gmail.com>
2018-11-05 19:34:39 -0200

    ModelMapperConfig

--- party-master/src/main/java/br/edu/ulbra/election/party/config/ModelMapperConfig.java
+++ party-master/src/main/java/br/edu/ulbra/election/party/config/ModelMapperConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
 
-    @Bean
+	@Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+    	ModelMapper modelMapper = new ModelMapper();
+    	modelMapper.getConfiguration().setAmbiguityIgnored(true);
+    	return modelMapper;
     }
 
 }
